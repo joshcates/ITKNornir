5d04f8fe6f61f46042151b69cc4b5e17b57f5660
Dženan Zukić <dzenan.zukic@kitware.com>
2024-08-30 10:28:25 -0400

    WIP: Reduce it to minimum reproducible case

---
+++ src/placeholder.cxx
@@ -0,0 +1,4 @@
+#include "NornirExport.h"
+
+// This file needs to have some definitions in order to create a .lib file.
+Nornir_EXPORT int placeholder = 0;
--- test/itkCompileTest.cxx
+++ test/itkCompileTest.cxx
@@ -1,15 +1,15 @@
 #include <list>
 #include <itkImageMaskSpatialObject.h>
 
-template <typename T>
-inline std::list<T>
-operator+(const T & a, const T & b)
-{
-  std::list<T> ab;
-  ab.push_back(a);
-  ab.push_back(b);
-  return ab;
-}
+//template <typename T>
+//inline std::list<T>
+//operator+(const T & a, const T & b)
+//{
+//  std::list<T> ab;
+//  ab.push_back(a);
+//  ab.push_back(b);
+//  return ab;
+//}
 
 int
 itkCompileTest(int argc, char * argv[])
